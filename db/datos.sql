1e84d245eecaadf7070d6361d5df50a27d5f0d2a
unknown <you@example.com>
2022-07-10 18:28:37 -0300

    Archivo .sql con datos fleteros

---
+++ db/datos.sql
@@ -0,0 +1,6 @@
+INSERT INTO `javatpdb`.`fletero` (`nombre`, `apellido`, `telefono`, `mail`) 
+VALUES 	('Gerardo', 'Zuqui', '3417841484', 'gzuqui@gmail.com'), 
+		('Bernardo', 'Perez', '3364457505', 'bepe90@gmail.com'),
+		('Mario', 'Gimenez', '3417800088', 'mgimenez@gmail.com'),
+		('Roberto', 'Lopez', '3416155045', 'roberto.lopez@gmail.com'),
+        ('Lucas', 'Marielli', '3364490499', 'lumarielli29@gmail.com');
